293f12188dbea0efdafe218ea94695dcfd41eb39
Loper <loper.t2h@gmail.com>
2012-02-15 00:37:12 +0100

    pobieranie kluczy i wyswietlanie naglowkow

--- Konfiguracja.py
+++ Konfiguracja.py
@@ -3,11 +3,14 @@
 
 class Konfiguracja(object):
     '''klasa Konfiguracja'''
+    
+    __ustawienia = {}
 
     def __init__(self, plik):
         self.__otworz_plik(plik)
     
     def __otworz_plik(self, konfig):
+        '''otwiera plik i odczytuje z niego konfigurację'''
         plik = open(konfig, 'r')
         plik = plik.readlines()
         for linia in plik:                   
@@ -15,5 +18,18 @@ class Konfiguracja(object):
                 continue
             if linia[0] in ('#', '/'):
                 continue
-            linia=linia.rstrip()
+            linia = linia.rstrip().split("=")
+            if len(linia) != 2:
+                continue
+            self.__dodaj_do_ustawien(linia[0], linia[1])
             
+        
+    def __dodaj_do_ustawien(self, klucz, wartosc):
+        '''dodaje pozycje do słownika ustawień'''
+        self.__ustawienia.update({klucz:wartosc})
+        
+    def podaj_wartosc(self, klucz):
+        try:
+            return self.__ustawienia[klucz]
+        except:
+            return "<brak klucza>"
